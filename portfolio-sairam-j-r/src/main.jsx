7176574640e8b55c9c39de5d42a8ec53eaf2e550
sairam5686 <jrsairam5686@gmail.com>
2025-10-06 22:51:06 +0530

    --project --c1/2

--- portfolio-sairam-j-r/src/main.jsx
+++ portfolio-sairam-j-r/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import App from './App.jsx'
+import Projects from './pages/Projects.jsx'
+
+
+const routes = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      },
+      {
+        path:'/projects',
+        element:<Projects />
+      }
+  ]
+  );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
