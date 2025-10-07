d2d007b36c4d2589f8cd296070ca8f4ce28fc8e2
sairam5686 <jrsairam5686@gmail.com>
2025-10-07 12:34:04 +0530

    --background --commit -main

--- portfolio-sairam-j-r/src/App.jsx
+++ portfolio-sairam-j-r/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import LiquidEther from './components/Background_main'
 
 const App = () => {
   return (
-    <div className='text-2xl'>thsi is app</div>
+    <div>
+      <LiquidEther 
+    colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
+    mouseForce={20}
+    cursorSize={100}
+    isViscous={false}
+    viscous={30}
+    iterationsViscous={32}
+    iterationsPoisson={32}
+    resolution={0.5}
+    isBounce={false}
+    autoDemo={true}
+    autoSpeed={0.5}
+    autoIntensity={2.2}
+    takeoverDuration={0.25}
+    autoResumeDelay={3000}
+    autoRampDuration={0.6} />
+    
+    </div>
   )
 }
 
